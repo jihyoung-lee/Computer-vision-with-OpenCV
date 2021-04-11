10e68dd3215df9a1eeb4bafef08b55c5921017eb
J.H.Lee <67559886+jihyoung-lee@users.noreply.github.com>
2021-04-11 22:43:55 +0900

    Add function

--- Computer Vision with Python3/trackbar.py
+++ Computer Vision with Python3/trackbar.py
@@ -1,13 +1,45 @@
 import cv2 as cv
 
+img = cv.imread('chess.jpg', cv.IMREAD_GRAYSCALE)
 
-# 트랙바 이벤트를 처리 할 콜백 함수
-def onChange(pos):
-    pass
+
+# onChange :  트랙바 이벤트를 처리 할 콜백 함수
+
+def onChange(dst):  # 적응적 이진화 콜백함수
+    blockSize = cv.getTrackbarPos('blockSize', 'Adaptive')
+    c = cv.getTrackbarPos('c', 'Adaptive')
+    if blockSize % 2 == 1:  # blockSize % 2 == 1 & blockSize > 1
+        # ADAPTIVE_THRESH_MEAN_C
+        # adaptive = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, blockSize, c)
+        # ADAPTIVE_THRESH_GAUSSIAN_C
+        adaptive = cv.adaptiveThreshold(img, 255, cv. ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, blockSize, c)
+        cv.imshow('Adaptive', adaptive)
+
+
+def Adaptive():  # 적응적 이진화
+    cv.namedWindow('Adaptive')
+    cv.createTrackbar('blockSize', 'Adaptive', 3, 255, onChange)
+    cv.createTrackbar('c', 'Adaptive', 0, 255, onChange)
+    cv.imshow('Adaptive', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def onChange2(dst):  # 이진화 콜백함수
+    low = cv.getTrackbarPos('threshold', 'binarization')  # 트랙바의 현재값을 가져와 임계값으로 사용할 수 있도록 함
+    ret, img_binary = cv.threshold(img, low, 255, cv.THRESH_BINARY_INV)  # THRESH_BINARY_INV : 반전된 마스크 이미지
+    cv.imshow('binarization', img_binary)
+
+
+def binarization():  # 이진화
+    cv.namedWindow('binarization')
+    cv.createTrackbar('threshold', 'binarization', 0, 255, onChange2)  # 트랙바 생성
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 def img_show():
-    img = cv.imread('chess.jpg', cv.IMREAD_GRAYSCALE)  # Grayscale
+    img2 = cv.imread('chess.jpg', cv.IMREAD_GRAYSCALE)  # Grayscale
     cv.imshow('window', img)
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -23,18 +55,4 @@ def img_show2():
     cv.destroyAllWindows()
 
 
-# 트랙바 함수
-def trackbar():
-    img = cv.imread('chess.jpg')
-    cv.namedWindow('binarization')
-    cv.createTrackbar('threshold', 'binarization', 0, 255, onChange)  # 트랙바 생성
-    while True:
-        low = cv.getTrackbarPos('threshold', 'binarization')  # 트랙바의 현재값을 가져와 임계값으로 사용할 수 있도록 함
-        ret, img_binary = cv.threshold(img, low, 255, cv.THRESH_BINARY_INV)  # THRESH_BINARY_INV : 반전된 마스크 이미지
-        cv.imshow('binarization', img_binary)
-        if cv.waitKey(1) & 0xFF == 27:  # esc 누르면 닫음
-            break
-    cv.destroyAllWindows()
-
-
-trackbar()
+Adaptive()
