4a1f1aac76c96f44a1e47f39807934fac61eb269
J.H.Lee <67559886+jihyoung-lee@users.noreply.github.com>
2021-04-29 23:40:43 +0900

    Update backprojection.py
    Face detection with Histogram Backprojection
    히스토그램 역투영을 이용한 얼굴 검출

--- Computer Vision with Python3/backprojection.py
+++ Computer Vision with Python3/backprojection.py
@@ -1,12 +1,22 @@
 import cv2
 
-img = cv2.imread('veteran.jpg', cv2.IMREAD_COLOR)
-mask = cv2.imread('model_face.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('veteran.jpg', cv2.IMREAD_COLOR)  # target img
+mask = cv2.imread('model_face.jpg', cv2.IMREAD_COLOR)  # mask img
+mask_hsv = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 
-def histogram():
-    channels = [0,1,2]
-    ranges = [0, 256, 0, 256]
-    hist = cv2.calcHist([img_hsv], channels, mask, [128, 128], ranges)
-    hist_norm = cv2.normalize(cv2.log(hist + 1), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+def back_projection():  # Histogram Backprojection
+    channels = [0, 1]
+    ranges = [0, 180, 0, 256]  # H = 0~180 , S = 0~256
+    mask_hist = cv2.calcHist([mask_hsv], channels, None, [180, 256], ranges)  # mask img 's histogram
+    cv2.normalize(mask_hist, mask_hist, 0, 255, cv2.NORM_MINMAX)
+    dst = cv2.calcBackProject([img_hsv], channels, mask_hist, ranges, 5)  # backprojection
+    ret, img_binary = cv2.threshold(dst, 0, 255, cv2.THRESH_BINARY +
+                                    cv2.THRESH_OTSU)  # Otsu binarization
+    cv2.imshow('Original', img)
+    cv2.imshow('Result', img_binary)
+    cv2.waitKey(0)
+
+
+back_projection()
